8641b9f6db379b8282ff859e37e9d31366f34956
Kurucz Laci <lacikurucz09@gmail.com>
2025-05-12 00:25:14 +0200

    cdk reel scroll

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,64 +1,30 @@
 import {
   AfterViewInit,
+  ChangeDetectionStrategy,
   Component,
   ElementRef,
   OnInit,
   ViewChild,
+  ViewEncapsulation,
 } from '@angular/core';
 import { TourVideoService } from '../../core/services/tour-video/tour-video.service';
 import { TourVideoCardComponent } from '../../layout/components/TourVideoCard/TourVideoCard.component';
 import { MatIconModule } from '@angular/material/icon';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [MatIconModule, TourVideoCardComponent],
+  encapsulation: ViewEncapsulation.None,
+  imports: [MatIconModule, TourVideoCardComponent, ScrollingModule],
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements OnInit {
   constructor(readonly tvService: TourVideoService) {}
 
   now = new Date();
 
   ngOnInit() {
     this.tvService.getTourVideos();
-  }
-
-  @ViewChild('videoReel') videoReel!: ElementRef;
-  isDown = false;
-  startX = 0;
-  scrollLeft = 0;
-
-  ngAfterViewInit() {
-    const reel = this.videoReel.nativeElement;
-
-    reel.addEventListener('mousedown', (e: MouseEvent) => this.startDrag(e));
-    reel.addEventListener('mouseleave', () => this.endDrag());
-    reel.addEventListener('mouseup', () => this.endDrag());
-    reel.addEventListener('mousemove', (e: MouseEvent) => this.drag(e));
-
-    reel.addEventListener('touchstart', (e: TouchEvent) => this.startDrag(e.touches[0]));
-    reel.addEventListener('touchend', () => this.endDrag());
-    reel.addEventListener('touchmove', (e: TouchEvent) => this.drag(e.touches[0]));
-  }
-
-  startDrag(e: MouseEvent | Touch) {
-    this.isDown = true;
-    this.startX = e.pageX - this.videoReel.nativeElement.offsetLeft;
-    this.scrollLeft = this.videoReel.nativeElement.scrollLeft;
-  }
-
-  endDrag() {
-    this.isDown = false;
-  }
-
-  drag(e: MouseEvent | Touch) {
-    if (!this.isDown) return;
-    if ('preventDefault' in e) {
-      e.preventDefault();
-    }
-    const x = e.pageX - this.videoReel.nativeElement.offsetLeft;
-    const walk = (x - this.startX) * 1.5;
-    this.videoReel.nativeElement.scrollLeft = this.scrollLeft - walk;
   }
 }
